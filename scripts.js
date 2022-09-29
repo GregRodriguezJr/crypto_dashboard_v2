7948aacf464546f4d27f599761bb56a82e3be887
Greg Rodriguez <greg.c.rodriguez.jr@gmail.com>
2022-09-28 21:47:52 -0500

    added renderCard function

--- scripts.js
+++ scripts.js
@@ -12,15 +12,23 @@ const getCoinGecko = async (endPoint) => {
     };
 };
 
-// Render to DOM
-{/* <li class="list-group-item">Cras justo odio</li> */}
+// Render trending coins to a card
+const renderCard = (coins) => {
+    coins.forEach((coin, index) => {
+        $('#trending-ul').append(`
+            <li class="list-group-item">
+                ${index + 1}. ${coin.item.name}
+            </li>
+        `)
+    });
+};
 
 // API call to get top 7 trending coins
 const getTrending = async (endPoint) => {
     try {
         const res = await fetch(baseUrl + endPoint);
         const data = await res.json();
-        console.log(data.coins);
+        renderCard(data.coins);
     } catch (error) {
         console.log(error);
     }
